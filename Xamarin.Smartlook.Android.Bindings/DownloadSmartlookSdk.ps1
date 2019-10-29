f0f30ea0b18ca08b8c2e8e783a1a8ecd23423627
Rafał Nowosielski <rafal@nowosielski.email>
2019-10-29 13:58:34 +0100

    Try to make it work on other platforms.

--- Xamarin.Smartlook.Android.Bindings/DownloadSmartlookSdk.ps1
+++ Xamarin.Smartlook.Android.Bindings/DownloadSmartlookSdk.ps1
@@ -1,1 +1,4 @@
-﻿Invoke-WebRequest -Uri https://sdk.smartlook.com/android/release/com/smartlook/recording/app/1.1.5-native/app-1.1.5-native.aar -OutFile "Jars/smartlook-1.1.5-native.aar"
+﻿if (!(Test-Path -Path "./Jars" )) {
+    New-Item -ItemType directory -Path "./Jars"
+}
+Invoke-WebRequest -Uri "https://sdk.smartlook.com/android/release/com/smartlook/recording/app/1.1.5-native/app-1.1.5-native.aar" -OutFile "./Jars/smartlook-1.1.5-native.aar"
--- Xamarin.Smartlook.iOS.Bindings/DownloadSmartlookSdk.ps1
+++ Xamarin.Smartlook.iOS.Bindings/DownloadSmartlookSdk.ps1
@@ -1,6 +1,5 @@
 ﻿$sdkUrl = "https://sdk.smartlook.com/ios/smartlook-ios-sdk-1.2.3.28.zip"
-$sdkZipFile = "smartlook-ios-sdk-1.2.3.28.zip"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($sdkUrl, $sdkZipFile)
+$sdkZipFile = "./smartlook-ios-sdk-1.2.3.28.zip"
+Invoke-WebRequest -Uri $sdkUrl -OutFile $sdkZipFile
 Expand-Archive $sdkZipFile -DestinationPath "." -Force
 Remove-Item –path $sdkZipFile
