6e16ecb45202c79e35eada81c39418a59638910b
Rafał Nowosielski <rafal@nowosielski.email>
2019-10-29 13:24:50 +0100

    Changes for cross platform

--- Xamarin.Smartlook.Android.Bindings/DownloadSmartlookSdk.ps1
+++ Xamarin.Smartlook.Android.Bindings/DownloadSmartlookSdk.ps1
@@ -1,3 +1,1 @@
-﻿$wc = New-Object System.Net.WebClient
-$wc.DownloadFile("https://sdk.smartlook.com/android/release/com/smartlook/recording/app/1.1.5-native/app-1.1.5-native.aar", "Jars/smartlook-1.1.5-native.aar")
-
+﻿Invoke-WebRequest -Uri https://sdk.smartlook.com/android/release/com/smartlook/recording/app/1.1.5-native/app-1.1.5-native.aar -OutFile "Jars/smartlook-1.1.5-native.aar"
